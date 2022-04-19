cf197c081ff468b3b5b58a29ace5fd30bbad5d54
creationk <creationk@users.noreply.github.com>
2022-04-19 12:50:19 +0530

    Collections
    Signed-off-by: abhinay-m <m.abhinay@gmail.com>

--- Collections/src/test/java/com/arrays/initialization/TestInitialization.java
+++ Collections/src/test/java/com/arrays/initialization/TestInitialization.java
@@ -6,31 +6,42 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 
 @Slf4j
-public class TestInitialization {
+class TestInitialization {
     @Test
-    public void initializeArray() {
+    void initializeArray() {
         int[] x = {2, 3, 4};
 
         int[] y = new int[]{3, 4, 5};
 
         //int[] y = new int[3]{3, 4, 5}; not allowed
 
-        int [] p = new int[3];
+        int[] p = new int[3];
         //p = {2,3,4}; not allowed
-        p = new int[]{3,4,5}; //allowed
+        p = new int[]{3, 4, 5}; //allowed
 
         int[] u = new int[6];
-        //Automatically initialized to 0
+        //All array elements automatically initialized to 0
 
-        log.info(String.valueOf(u[5]));
+        log.info("u[5] = {}", u[5]);
 
         int[][] a = new int[2][];
         log.info(Arrays.toString(a[0])); //initialized to null
+        //log.info(String.valueOf(a[0][0])); //Cannot load from int array because "a[0]" is null
+        a[0] = new int[1];
+        log.info("a[0][0] ={}", (a[0][0])); //Cannot load from int array because "a[0]" is null
 
-        int[][]b={{},{},{}};
+        int[][] b = {{}, {}, {}};
 
-        for (int[] i:b){
+        for (int[] i : b) {
             log.info(Arrays.toString(i));
+        }
+
+        int [][]tw = new int[][]{{1},{2,3},{4,5,6}};
+
+        for (int[] i: tw){
+            for(int j: i){
+                log.info("int[i][j]={}",j);
+            }
         }
 
     }
